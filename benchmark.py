2a42aee588b6acab8b8694a0be86ab455bce93e7
RiccoSoares <52585330+RiccoSoares@users.noreply.github.com>
2021-05-03 23:38:59 -0300

    Add files via upload

--- benchmark.py
+++ benchmark.py
@@ -27,7 +27,7 @@ if not os.path.isdir(outputDirPath):
 assert (isCacheParameter and isCpuParameter) is False #the parameter can't be simoutaneously a cache and cpu parameter
 parameterValue = initialValue
 
-for index, algPath in algorithmFilePaths:
+for index, algPath in enumerate(algorithmFilePaths):
 
 	algName = algNames[index]
 	print("Benchmarking Algorithm: " + algName)
